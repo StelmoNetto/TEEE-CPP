4fb9cca01ff1e4bc81dbb2cbfed39ba885488374
StelmoNetto <stelmo.netto@ufma.br>
2023-08-23 13:02:32 -0300

    Correcao: adicao das variaveis inteiras usadas na razao e do comentario da sintaxa de casting

--- parte_1/introducao/conversoes_de_tipo.cpp
+++ parte_1/introducao/conversoes_de_tipo.cpp
@@ -5,7 +5,8 @@ int main()
     int var = 2.7; //var armazenará apenas a parte inteira, 2.
     float razao = 1 / 2; /*Divisão de dois inteiros, o resultado é o inteiro zero.*/
 
-    static_cast<tipoDaConversao>(variavel)
+    int intVariavel1{1}, intVariavel2{2};
+    //static_cast<tipoDaConversao>(variavel)
     double variavel = static_cast<double>(intVariavel1) / intVariavel2; //Verbosa
     float v = 17 / (float)5; //Forma antiga – não recomendado
 }

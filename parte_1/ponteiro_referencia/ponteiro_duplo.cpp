0eb67c64979f5d47a615013d61d7b6b2e367cd0a
StelmoNetto <stelmo.netto@ufma.br>
2023-09-06 13:12:09 -0300

    Adicao de comentarios

--- parte_1/ponteiro_referencia/ponteiro_duplo.cpp
+++ parte_1/ponteiro_referencia/ponteiro_duplo.cpp
@@ -5,9 +5,9 @@ int main()
 {
     int** handle { nullptr }; //Na pilha
 
-    handle = new int*; //Na heap
+    handle = new int*; //Na heap. Aponta para um ponteiro inteiro
 
-    *handle = new int; //Na heap
+    *handle = new int; //Na heap. Acessa o conteúdo (ponteiro) para qual aponta
 
     cout << **handle << endl;
 }

9ff85695f7ab0fbd389062d2453f696d801f88bd
StelmoNetto <stelmo.netto@ufma.br>
2023-09-06 14:19:37 -0300

    Refatoracao: melhoria do nome da referencia

--- parte_1/ponteiro_referencia/endereco_da_variavel_referenciada.cpp
+++ parte_1/ponteiro_referencia/endereco_da_variavel_referenciada.cpp
@@ -5,8 +5,8 @@ int main()
 {
   int x = 3;
 
-  int& referenciaDeX = x;
-  int* apontaParaX = &referenciaDeX; //Endereço da referência é o endereço de x
+  int& referenciaParaX = x;
+  int* apontaParaX = &referenciaParaX; //Endereço da referência é o endereço de x
 
   *apontaParaX = 100; //Alterando x para 100
 

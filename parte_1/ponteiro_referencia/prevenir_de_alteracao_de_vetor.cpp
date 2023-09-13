82985af5769478ece81a6c45cdeb05746ad95c79
StelmoNetto <stelmo.netto@ufma.br>
2023-09-13 13:51:17 -0300

    Adicionado alternativa para a definicao do ponteiro de constantes inteiras

--- parte_1/ponteiro_referencia/prevenir_de_alteracao_de_vetor.cpp
+++ parte_1/ponteiro_referencia/prevenir_de_alteracao_de_vetor.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int vetor[]{ 3,4,5 };
 
-    const int* ptrParaInteirosConstantes = vetor;
+    const int* ptrParaInteirosConstantes = vetor; //Ou: int const* ptrParaInteirosConstantes = vetor;
 
     ptrParaInteirosConstantes[0] = 2; // Erro: não pode alterar os elementos do vetor
 }

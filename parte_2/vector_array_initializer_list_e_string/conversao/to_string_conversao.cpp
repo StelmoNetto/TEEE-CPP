5b920ff4d4072edd0525ddd0c1027de81c5b1186
StelmoNetto <stelmo.netto@ufma.br>
2023-11-01 17:03:04 -0300

    Atualizacao: expancao do exemplo para incluir numeros na base binaria, octal e hexadecimal

--- parte_2/vector_array_initializer_list_e_string/conversao/to_string_conversao.cpp
+++ parte_2/vector_array_initializer_list_e_string/conversao/to_string_conversao.cpp
@@ -6,6 +6,17 @@ using namespace std::string_literals;
 int main()
 {
     auto si = std::to_string(42);
-
     cout << si.c_str() << endl;
+
+    si = std::to_string(0b10);
+    cout << si.c_str() << endl;    
+
+    si = std::to_string(042);
+    cout << si.c_str() << endl;    
+
+    si = std::to_string(0x42);
+    cout << si.c_str() << endl;
+
+    auto sd = std::to_string(42.1);
+    cout << sd.c_str() << endl;        
 }

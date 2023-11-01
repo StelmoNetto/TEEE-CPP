00f466acc8252487f9425cef32f952244cabde90
StelmoNetto <stelmo.netto@ufma.br>
2023-11-01 17:07:22 -0300

    Exemplo de string numerica fora do intervalo da conversao

---
+++ parte_2/vector_array_initializer_list_e_string/conversao/stoi_out_of_range.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using std::string, std::cout, std::endl;
+using namespace std::string_literals;
+
+int main()
+{
+    string str_dec = "2147483648";
+    string::size_type sz;
+
+    int i_dec = stoi(str_dec, &sz);
+
+    cout << str_dec << ": " << i_dec << " and [" << str_dec.substr(sz) << "]\n";
+}

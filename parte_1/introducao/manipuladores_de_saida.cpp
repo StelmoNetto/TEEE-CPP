5ff582baae564397b3739c35c6cb60ac9195499c
StelmoNetto <92476998+StelmoNetto@users.noreply.github.com>
2025-04-09 13:57:20 -0300

    Atualizacao
    Adicao de mais um bloco mostrando que a precisao para numeros nao fixados define a lagura, que e soma da parte inteira somada aos digitos decimais

--- parte_1/introducao/manipuladores_de_saida.cpp
+++ parte_1/introducao/manipuladores_de_saida.cpp
@@ -1,20 +1,27 @@
-
 #include<iomanip>
 #include<iostream>
 using namespace std; //Inclui toda a biblioteca-padrão do namespace std no código fonte
 
 int main()
 {
-  {
+	{
 		int a;
-		cin >> hex >> a;
-		std::cout << "OK " << std::showpoint << a << std::endl;
+		cin >> hex >> a; //Espera um hexadecimal como entrada
+		std::cout << "OK " << a << std::endl;
 	}
 
 	{
 		bool a;
-		cin >> a;
+		cin >> a; //0: false / 1-X: true
 		cout << boolalpha << a << endl;
+	}
+
+	{
+		auto preco{ 7.753 };
+		cout << showpoint; //Sempre mostra o ponto, mesmo sem decimal do número real
+		cout << setprecision(2); //Precisão de dois digitos de largura (parte inteira + digitos decimais), quando não está em notação fixada. Incluir <iomanip>.
+		cout << preco << endl;
+		cout << showpos << preco << endl;
 	}
 
 	{
@@ -34,15 +41,15 @@ int main()
 		cout << setw(6); //Largura de saída de 6 espaços
 		cout << setfill('*'); //Caractere de preenchimento dos espaços restantes
 		cout << preco << endl;
-    cout << std::showpos << preco << endl; //Mostra sinal '+' via flags std
+		cout << std::showpos << preco << endl; //Mostra sinal '+' via flags std
 	}
 
-  //Exemplo de formatação da saída em ponto flutuante com ios
-  {
+	//Exemplo de formatação da saída em ponto flutuante com ios
+	{
 		auto preco{ 7.753 };
-    cout.setf(ios::fixed); //Notação não científica de números reais
-    cout.setf(ios::showpoint); //Sempre mostra o ponto, mesmo sem decimal
-    cout.precision(2); //Duas casas decimais
-    cout << preco << endl;
-  }
+		cout.setf(ios::fixed); //Notação não científica de números reais
+		cout.setf(ios::showpoint); //Sempre mostra o ponto, mesmo sem decimal
+		cout.precision(2); //Duas casas decimais
+		cout << preco << endl;
+	}
 }
